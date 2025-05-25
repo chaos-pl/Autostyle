1df33d08f017fe1c4ecbe46bc32e284077edddad
Imanol <imanol10albiter@gmail.com>
2025-05-24 21:34:23 -0600

    Creacion de layout dashboard

--- resources/views/categorias/index.blade.php
+++ resources/views/categorias/index.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.app')
+@extends('layouts.dashboard')
 
-@section('content')
+@section('dashboard-content')
     <div class="p-4">
         <h3 class="fw-bold">Categorías</h3>
         <p>Selecciona una categoría del menú lateral para ver sus productos.</p>

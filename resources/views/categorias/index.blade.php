c298ac3f6823754ad87a666ce67d636161826842
Axolotl GG <emigm144@gmail.com>
2025-05-24 19:21:21 -0600

    arreglo de vista categorias

---
+++ resources/views/categorias/index.blade.php
@@ -0,0 +1,8 @@
+@extends('home.app')
+
+@section('dashboard-content')
+    <div class="p-4">
+        <h3 class="fw-bold">Categorías</h3>
+        <p>Selecciona una categoría del menú lateral para ver sus productos.</p>
+    </div>
+@endsection
